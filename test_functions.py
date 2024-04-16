18ee4b09d2430ed677676154690a3da7d703150e
Huzaifa-Arif <91891697+Huzaifa-Arif@users.noreply.github.com>
2024-04-16 17:54:52 -0400

    Update test_functions.py

--- test_functions.py
+++ test_functions.py
@@ -24,7 +24,7 @@ import numpy as np
 dataset = ['ADULT'] #,#'COMPAS']
 model = ['LR'] #'LR']  # 'NN' --> KRTD and KRTWD
 #methods = ['KRTWD','KRTD']#'KRTD' , # 'FairFed_w_FairBatch_kernel'
-methods = ['KRTD'] # 'KRTWD','Central','FairFed_w_FairBatch', 'FedAvg','MinMax''FairFed_w_FairBatch_kernel'
+methods = ['KRTWD'] # 'KRTWD','Central','FairFed_w_FairBatch', 'FedAvg','MinMax''FairFed_w_FairBatch_kernel'
 #methods = ['KRTWD']
 dist = ['Non-IID'] #'IID'] 
 
